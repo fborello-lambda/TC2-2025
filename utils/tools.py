d22b72ff2132aaf1597d8c9564bc2587868b788d
Federico Borello <156438142+fborello-lambda@users.noreply.github.com>
2025-05-26 00:42:55 -0300

    Ts5 (#5)
    * feat(TS5): add template
    
    * feat(TS5): add pdf
    
    * feat(TS5): add upto 3
    
    * feat(TS5): complete TS5

---
+++ utils/tools.py
@@ -0,0 +1,25 @@
+import sympy as sp
+from scipy import signal as sig
+from IPython.display import display, Markdown
+
+
+def bessel_thomson_sos_sects(b, a):
+    sos = sig.tf2sos(b, a, analog=True)
+    s = sp.Symbol("s")
+
+    ret = []
+
+    for i, section in enumerate(sos, 1):
+        _, _, _, a0, a1, a2 = section
+
+        num = a2
+        den = a0 * s**2 + a1 * s + a2
+
+        # Make denominator monic if a0 is not 1 or 0
+        if a0 != 0 and a0 != 1:
+            num = num / a0
+            den = den / a0
+        H_i_monic = sp.simplify(num / den)
+        ret.append(H_i_monic)
+        display(Markdown(f"$$H_{{{i}}}(s) = {sp.latex(H_i_monic.evalf(3))}$$"))
+    return ret
